eb8673ca46ed47d7adcd73c6e633f5e278eeb205
Micah Lanier <mlanier@mlanieriMac.local>
2016-04-29 15:56:12 -0500

    add User and ProfileVC

---
+++ Finish It/ProfileViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileViewController.swift
+//  Finish It
+//
+//  Created by Micah Lanier on 4/29/16.
+//  Copyright © 2016 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    let imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        profileImageView.layer.cornerRadius = profileImageView.frame.width/1.8
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onImageButtonTapped(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImageView.contentMode = .ScaleAspectFill
+            profileImageView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onFinishedButtonTapped(sender: AnyObject) {
+        let user = User(userName: usernameTextField.text!, image: profileImageView.image!)
+        print(user.name)
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+   
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+    
+
+}
---
+++ Finish It/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  Finish It
+//
+//  Created by Micah Lanier on 4/29/16.
+//  Copyright © 2016 Micah Lanier Design and Illustration. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var name: NSString!
+    var profileImage: UIImage!
+    
+    init(userName: String, image: UIImage) {
+        super.init()
+        self.name = userName
+        self.profileImage = image
+        save()
+    }
+
+    func save() {
+        if let image = profileImage {
+            if let data = UIImagePNGRepresentation(image) {
+                let filename = getDocumentsDirectory().stringByAppendingPathComponent("copy.png")
+                data.writeToFile(filename, atomically: true)
+            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+}
